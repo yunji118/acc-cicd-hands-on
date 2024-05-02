61ed279e3594520288b945fea57ce2a77f3cd4a2
Yunji Ha <yunji118@ewhain.net>
2024-05-02 22:21:32 +0900

    Update prod.py

--- djangoProject/settings/prod.py
+++ djangoProject/settings/prod.py
@@ -1,16 +1,13 @@
 from .base import *  # noqa
-
+import os
 DEBUG = False
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+DEBUG = True
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),
-        'OPTIONS': {"charset": "utf8mb4"},
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR , "db.sqlite3"),
     }
 }
